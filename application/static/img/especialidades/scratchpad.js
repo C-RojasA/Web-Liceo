7d2514c8d81a92c3bbf6a6a3bfa424884bfeda08
C-RojasA <cristian@localhost.localdomain>
2017-06-23 00:09:29 -0400

    Modified Careers.html 	modified:   application/static/css/style.css 	modified:   application/static/html/careers.html 	modified:   application/static/img/especialidades/construccion.jpg

--- application/static/img/especialidades/scratchpad.js
+++
@@ -1,7 +0,0 @@
-
-var number = 46786.62; 
-// sin parámetros será el formato por defecto de tu navegador 
-console.log("Formato automático --- " + number.toLocaleString()); 
-// le puedes pasar un código de locale específico 
-console.log("Formato en EE.UU. ---- " + number.toLocaleString("en-US")); 
-console.log("Formato de España ---- " + number.toLocaleString("es-ES"));
